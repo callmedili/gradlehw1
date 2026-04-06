dd34f9e7fe5efd568f0c3c51c0931cf5217ebbb3
Dilya test <dilya.yusufova@gmail.com>
2026-04-06 18:31:11 +0500

    Add TestNG test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,37 +1,76 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateForAmountBelowBoundary() {
+    public void shouldCalculateForAmountBelowBoundary() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(900);
         int expected = 100;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    void shouldCalculateForAmountAboveBoundary() {
+    public void shouldCalculateForAmountAboveBoundary() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1100);
         int expected = 900;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
+    public void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 }
+
+
+//package ru.netology.service;
+//
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class CashbackHackServiceTest {
+//
+//    @Test
+//    void shouldCalculateForAmountBelowBoundary() {
+//        CashbackHackService service = new CashbackHackService();
+//
+//        int actual = service.remain(900);
+//        int expected = 100;
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void shouldCalculateForAmountAboveBoundary() {
+//        CashbackHackService service = new CashbackHackService();
+//
+//        int actual = service.remain(1100);
+//        int expected = 900;
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
+//        CashbackHackService service = new CashbackHackService();
+//
+//        int actual = service.remain(1000);
+//        int expected = 0;
+//
+//        assertEquals(expected, actual);
+//    }
+//}
